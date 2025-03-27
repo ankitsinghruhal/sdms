09250ce13fa98f7b2555864d21bc8156d9602ad3
Ankit Singh Ruhal <163099176+ankitsinghruhal@users.noreply.github.com>
2025-03-27 15:15:34 +0530

    commit

--- src/com/application/ContactUs.java
+++ src/com/application/ContactUs.java
@@ -7,6 +7,19 @@ public class ContactUs
 {
     public static void main(String[] args)
     {
-        
+        JFrame welcomeScreen = new JFrame("CDMS");
+        welcomeScreen.setSize(500,100);
+        welcomeScreen.setResizable(false);
+        welcomeScreen.setLocationRelativeTo(null);
+        welcomeScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel welcomeMessage = new JLabel("contact us at : registrar@dcrustm.org \n Telephone No. +91-130-2484005.");
+        welcomeMessage.setHorizontalAlignment(JLabel.CENTER);
+        welcomeMessage.setFont(new Font("Arial" , Font.PLAIN, 15));
+        welcomeScreen.add(welcomeMessage);
+
+        LoginScreen.main(args);
+
+        welcomeScreen.setVisible(true);
     }
 }
