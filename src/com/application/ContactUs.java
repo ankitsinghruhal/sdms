bb1be7ee6857325e64cc9a22ea3278226bab6306
Ankit Singh Ruhal <163099176+ankitsinghruhal@users.noreply.github.com>
2025-03-27 15:05:26 +0530

    commit

---
+++ src/com/application/ContactUs.java
@@ -0,0 +1,12 @@
+import com.application;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class ContactUs
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
