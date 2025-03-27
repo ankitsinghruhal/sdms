1f7439f40d6ba03f9ea1b81fa6774c17588491db
Ankit Singh Ruhal <163099176+ankitsinghruhal@users.noreply.github.com>
2025-03-27 13:57:44 +0530

    initial commit

---
+++ src/com/application/LoginScreen.java
@@ -0,0 +1,35 @@
+package com.application;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.event.KeyEvent;
+
+public class LoginScreen
+{
+    public static void main(String[] args)
+    {
+        JFrame loginScreen = new JFrame("CDMS - Login");
+        loginScreen.setSize(800,500);
+        loginScreen.setResizable(false);
+        loginScreen.setLocationRelativeTo(null);
+        loginScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JTabbedPane userpane = new JTabbedPane();
+        userpane.setSize(700,400);
+
+        JPanel LoginPanel = new JPanel(new GridLayout(3,2,5,5));
+        JPanel SignUpPanel = new JPanel(new GridLayout(3,2,5,5));
+
+        userpane.addTab("Login", LoginPanel);
+        userpane.setMnemonicAt(0, KeyEvent.VK_1);
+        userpane.addTab("Sign Up", SignUpPanel);
+        userpane.setMnemonicAt(1, KeyEvent.VK_1);
+        
+        loginScreen.setContentPane(userpane);
+        loginScreen.setVisible(true);
+    }   
+}
---
+++ src/com/application/WelcomeScreen.java
@@ -0,0 +1,26 @@
+package com.application;
+
+import java.awt.Font;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class WelcomeScreen
+{
+    public static void main(String[] args)
+    {
+        JFrame welcomeScreen = new JFrame("CDMS");
+        welcomeScreen.setSize(500,100);
+        welcomeScreen.setResizable(false);
+        welcomeScreen.setLocationRelativeTo(null);
+        welcomeScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel welcomeMessage = new JLabel("welcome to College Data management System.");
+        welcomeMessage.setHorizontalAlignment(JLabel.CENTER);
+        welcomeMessage.setFont(new Font("Arial" , Font.PLAIN, 15));
+        welcomeScreen.add(welcomeMessage);
+
+        LoginScreen.main(args);
+
+        welcomeScreen.setVisible(true);
+    }
+}
